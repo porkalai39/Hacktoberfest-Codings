8651f07110f0f4ca600e41078c98327099165dee
porkalai39 <porkalaiselvi241@gmail.com>
2024-10-02 14:31:13 +0530

    code added

---
+++ EvenOdd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>  
+  
+int main() {  
+int number;  
+  
+printf("Enter a number: ");  
+scanf("%d", &number);  
+  
+    if (number % 2 == 0) {  
+printf("%d is an even number.\n", number);  
+    } else {  
+printf("%d is an odd number.\n", number);  
+    }  
+  
+    return 0;  
+}  
